074e5b3d1b7f12b89c931c180914e0bbd61cb296
jaeirc <jaime.acevedo@gmail.com>
2015-10-13 20:08:04 -0300

    Cambios por nueva estructura de DB

--- pages_informes/default.php
+++ pages_informes/default.php
@@ -5,7 +5,7 @@ include_once "../recursos/zhi/CreaConnv2.php";
 
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='1' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -29,7 +29,7 @@ if($result = $mysqli->query($sql))
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Tº ambiental sobre cuba 1</h5>
 	 		<button type="button" class="btn btn-info btn-lg btn-block">
-	 			<?php 
+	 			<?php
 	 			echo $data[infoData];
 	 			?>ºC</button>
 	 		<h6>Fecha última medición:
@@ -40,7 +40,7 @@ if($result = $mysqli->query($sql))
 <?php
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='4' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -51,23 +51,23 @@ if($result = $mysqli->query($sql))
 	$result->free();
 }
 
-?>	 	
+?>
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Tº ambiental sobre cuba 2 (a)</h5>
 	 		<button type="button" class="btn btn-info btn-lg btn-block">
-	 			<?php 
-	 			echo $data[infoData]; 
+	 			<?php
+	 			echo $data[infoData];
 	 			?>ºC</button>
-	 		<h6>Fecha última medición: 
-	 			<?php 
-	 			echo $data[timeData]; 
+	 		<h6>Fecha última medición:
+	 			<?php
+	 			echo $data[timeData];
 	 			?></h6>
 	 	</div>
 
 <?php
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='6' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -78,20 +78,20 @@ if($result = $mysqli->query($sql))
 	$result->free();
 }
 
-?>	 	
+?>
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Tº ambiental sobre cuba 2 (b)</h5>
 	 		<button type="button" class="btn btn-info btn-lg btn-block">
-	 			<?php 
-	 			echo $data[infoData]; 
+	 			<?php
+	 			echo $data[infoData];
 	 			?>ºC</button>
-	 		<h6>Fecha última medición: 
-	 			<?php 
-	 			echo $data[timeData]; 
-	 			?></h6>	 		
-	 	</div>	 	
+	 		<h6>Fecha última medición:
+	 			<?php
+	 			echo $data[timeData];
+	 			?></h6>
+	 	</div>
 	</div>
-	
+
  	<div class="row bs-sidenav">
  		<div class="col-md-12">
  			<h4>Temperatura cubas</h4>
@@ -100,7 +100,7 @@ if($result = $mysqli->query($sql))
 <?php
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='2' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -111,28 +111,28 @@ if($result = $mysqli->query($sql))
 	$result->free();
 }
 
-?>		
+?>
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Cuba 1 - 40.000 lts</h5>
 	 		<button type="button" class="btn btn-success btn-lg btn-block">
-	 			<?php 
-	 			echo $data[infoData]; 
+	 			<?php
+	 			echo $data[infoData];
 	 			?>ºC</button>
-	 		<h6>Fecha última medición: 
-	 			<?php 
-	 			echo $data[timeData]; 
-	 			?></h6>	
-	 			</br>	
+	 		<h6>Fecha última medición:
+	 			<?php
+	 			echo $data[timeData];
+	 			?></h6>
+	 			</br>
 			    <label>
 			      <input type="checkbox" name="rf-switch-1"> &nbsp Válvula de refrigeración
 			    </label>
 	 	</div>
 	 	<div class="col-md-4">
- 		 
+
 	 	</div>
 	 	<div class="col-md-4">
- 		
-	 	</div>	 	
+
+	 	</div>
 	 </div>
 
  	<div class="row bs-sidenav">
@@ -143,7 +143,7 @@ if($result = $mysqli->query($sql))
 <?php
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='3' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -154,23 +154,23 @@ if($result = $mysqli->query($sql))
 	$result->free();
 }
 
-?>		
+?>
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Humedad 1</h5>
 	 		<button type="button" class="btn btn-success btn-lg btn-block">
-	 			<?php 
-	 			echo $data[infoData]; 
+	 			<?php
+	 			echo $data[infoData];
 	 			?>%</button>
-	 		<h6>Fecha última medición: 
-	 			<?php 
-	 			echo $data[timeData]; 
-	 			?></h6>	 		
+	 		<h6>Fecha última medición:
+	 			<?php
+	 			echo $data[timeData];
+	 			?></h6>
 	 	</div>
 
 <?php
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='5' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -181,24 +181,24 @@ if($result = $mysqli->query($sql))
 	$result->free();
 }
 
-?>	
+?>
 
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Humedad 2</h5>
 	 		<button type="button" class="btn btn-success btn-lg btn-block">
-	 			<?php 
-	 			echo $data[infoData]; 
+	 			<?php
+	 			echo $data[infoData];
 	 			?>%</button>
-	 		<h6>Fecha última medición: 
-	 			<?php 
-	 			echo $data[timeData]; 
-	 			?></h6>	 		
+	 		<h6>Fecha última medición:
+	 			<?php
+	 			echo $data[timeData];
+	 			?></h6>
 	 	</div>
 
 <?php
 $sql = "select infoData, convert_tz(timeData,'+00:00','-03:00') as timeData from Data where Device_idDevice='7' order by timeData desc limit 1";
 
-if($result = $mysqli->query($sql))
+if($result = $data_mysqli->query($sql))
 {
 	if ($debug)
 	{
@@ -209,20 +209,19 @@ if($result = $mysqli->query($sql))
 	$result->free();
 }
 
-?>	
+?>
 
 	 	<div class="col-md-4">
 	 		<h5 class="text-center">Humedad 3</h5>
 	 		<button type="button" class="btn btn-success btn-lg btn-block">
-	 			<?php 
-	 			echo $data[infoData]; 
+	 			<?php
+	 			echo $data[infoData];
 	 			?>%</button>
-	 		<h6>Fecha última medición: 
-	 			<?php 
-	 			echo $data[timeData]; 
-	 			?></h6>	 		
-	 	</div> 	
+	 		<h6>Fecha última medición:
+	 			<?php
+	 			echo $data[timeData];
+	 			?></h6>
+	 	</div>
 	 </div>
-	 <br> 	
+	 <br>
 </div><!-- col-md-11 -->
-
--- recursos/zhi/CreaConnv2.php
+++ recursos/zhi/CreaConnv2.php
@@ -6,6 +6,7 @@ $user = $ini_array['user'];
 $passwd = $ini_array['password'];
 $host = $ini_array['host'];
 $bd = $db = $ini_array['schema'];
+$data_db = $data_bd = $ini_array['data'];
 
 $mysqli = new mysqli($host, $user, $passwd, $bd);
 if ($mysqli->connect_errno) {
@@ -15,6 +16,16 @@ if ($mysqli->connect_errno) {
 $mysqli = new mysqli($host, $user, $passwd, $bd, $ini_array['port']);
 if ($mysqli->connect_errno) {
     echo "Fallo al contenctar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+$data_mysqli == new mysqli($host, $user, $passwd, $data_db);
+if ($data_mysqli->connect_errno) {
+    echo "Fallo al contenctar a MySQL: (" . $data_mysqli->connect_errno . ") " . $data_mysqli->connect_error;
+}
+
+$data_mysqli = new mysqli($host, $user, $passwd, $data_bd, $ini_array['port']);
+if ($data_mysqli->connect_errno) {
+    echo "Fallo al contenctar a MySQL: (" . $data_mysqli->connect_errno . ") " . $data_mysqli->connect_error;
 }
 
 ?>

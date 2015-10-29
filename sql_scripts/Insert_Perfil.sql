4033347925c6a6f6a1f355b22f2d1d2b82f5b531
Francisco Morán M <fjmoran@gmail.com>
2015-10-29 16:11:27 -0300

    Limpieza del directorio de scripts

---
+++ sql_scripts/Insert_Perfil.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM Sistema_WineBerry.Perfil
+LIMIT 0, 1000
+
+-- Date: 2015-10-29 16:07
+*/
+INSERT INTO `Perfil` (`idPerfil`,`nombrePerfil`,`descripcionPerfil`,`activoPerfil`) VALUES (1,'Administrador','Perfil de Administrador con permisos sobre todo',1);
+INSERT INTO `Perfil` (`idPerfil`,`nombrePerfil`,`descripcionPerfil`,`activoPerfil`) VALUES (2,'Visualizador','Puede ver la info del sistema',1);
---
+++ sql_scripts/Insert_Usuarios.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM Sistema_WineBerry.Usuario
+LIMIT 0, 1000
+
+-- Date: 2015-10-29 16:08
+*/
+INSERT INTO `Usuario` (`idUsuario`,`nombreUsuario`,`userUsuario`,`claveUsuario`,`correoUsuario`,`telefonoUsuario`,`celularUsuario`,`lastloginUsuario`,`activoUsuario`,`Perfil_idPerfil`) VALUES (1,'Administrador','root','0553a463d0fe226e64d8dba14c4e8033dc81ce23','root@localhost.localdomain',NULL,NULL,NULL,1,1);

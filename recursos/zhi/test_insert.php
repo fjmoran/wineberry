1585ae41e96cf165c183ea5559a8403a6f9be7f1
jaeirc <jaime.acevedo@gmail.com>
2015-10-28 21:35:55 -0300

    Tabla Switching
    Se arregla problema de mayusculas

--- recursos/zhi/test_insert.php
+++ recursos/zhi/test_insert.php
@@ -2,7 +2,7 @@
 //ini_set('display_errors', 1);
 include "CreaConnv2.php";
 
-$sql = "INSERT INTO switching (Switch_idSwitch,estadoSwitching,fechahoracambioSwitching) values (1,1,now())";
+$sql = "INSERT INTO Switching (Switch_idSwitch,estadoSwitching,fechahoracambioSwitching) values (1,1,now())";
 
 $rs = $data_mysqli->query($sql);
 if (!$rs){

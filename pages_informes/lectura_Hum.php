44b90ee466e0c3a962ebbb899fd6e8e8cbaf1412
jaeirc <jaime.acevedo@gmail.com>
2016-12-27 12:05:59 -0300

    Error Columna

--- pages_informes/lectura_Hum.php
+++ pages_informes/lectura_Hum.php
@@ -21,7 +21,7 @@ if (!(empty($DEVICE))) {
 	$query .= " WHERE DatosHumDevId = '$DEVICE' ";
 }
 
-$query .= " ORDER BY DatosDateTime DESC LIMIT 1";
+$query .= " ORDER BY DatosHumDateTime DESC LIMIT 1";
 
 $results = $data_mysqli->query($query);
 if ($results) {

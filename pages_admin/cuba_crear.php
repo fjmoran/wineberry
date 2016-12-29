d5ed977902c6354fc9f850e5b7b0dd45a9d1ae0f
jaeirc <jaime.acevedo@gmail.com>
2016-12-29 11:21:17 -0300

    se cambia la tabla

--- pages_admin/cuba_crear.php
+++ pages_admin/cuba_crear.php
@@ -5,9 +5,7 @@
 
 <?php 
 
-require_once("CreaConnv2.php");
-
-$_GET['table'] = $data_bd.".Cuba";
+$_GET['table'] = "Data_WineBerry".".Cuba";
 $_GET['select'] = "nombreCuba as Cuba, ubicacionCuba as Ubicacion, activoCuba as Estado";
 $_GET['jquery'] = "$('#cuerpo').load('pages_admin/cuba_mod.php');";
 
